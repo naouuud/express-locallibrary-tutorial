95a83e0e508e7a0dd9985e3174e6cbea9ce94b07
Nour Aoude <naoude@gmail.com>
2024-01-03 22:19:50 +0200

    Adjust Rate Limiter code

--- app.js
+++ app.js
@@ -46,9 +46,10 @@ app.use(
   })
 );
 const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000,
+  windowMs: 1 * 60 * 1000, // 1 minute
   max: 20,
 });
+// Apply rate limiter to all requests
 app.use(limiter);
 
 // view engine setup
